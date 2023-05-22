afbc8bed5aa0a1f1f11c7b46c016d5bda5e64cc8
AngelikaKh <a.khodzhaian@gmail.com>
2023-05-22 12:47:48 +0400

    page transition change

--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,20 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-link1 = "http://selenium1py.pythonanywhere.com/"
-link2 = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+link = "http://selenium1py.pythonanywhere.com/"
+login_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+handbooks_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link1)
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link1)
+    page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_can_be_sure_its_login_page(browser):
-    page = LoginPage(browser, link2)
-    page.open()
-    page.should_be_login_page()

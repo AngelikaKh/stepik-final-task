1b7c712adac04e718d9f4fc407705c6fa2fc7316
AngelikaKh <a.khodzhaian@gmail.com>
2023-05-23 00:42:49 +0400

    adding 1 xfail test

--- test_product_page.py
+++ test_product_page.py
@@ -13,8 +13,7 @@ from pages.product_page import ProductPage
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
                                                "/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-                                               "/?promo=offer9", marks=pytest.mark.xfail)])
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()

5ed3e3724a8ed03e5ec6e9e6ccdf36ae6c92a355
AngelikaKh <a.khodzhaian@gmail.com>
2023-05-23 02:31:47 +0400

    adding a plug

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,3 +4,8 @@ from .base_page import BasePage
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
+
+# or this way
+
+# class MainPage(BasePage):
+#     pass

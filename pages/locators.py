3d2418219aad62746b1056a819c84a6bd9636115
AngelikaKh <a.khodzhaian@gmail.com>
2023-05-22 22:06:55 +0400

    adding product to basket

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+
+    ITEM_NAME = (By.XPATH, "/html[1]/body[1]/div[2]/div[1]/div[2]/div[2]/article[1]/div[1]/div[2]/h1[1]")
+    ADDED_ITEM = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+
+    PRODUCT_PRICE = (By.XPATH, "/html[1]/body[1]/div[2]/div[1]/div[2]/div[2]/article[1]/div[1]/div[2]/p[1]")
+    TOTAL_PRICE = (By.XPATH, "/html/body/div[2]/div/div[1]/div[3]/div/p[1]/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket.click()
+
+    def should_be_correct_product(self):
+        added_item_text = self.browser.find_element(*ProductPageLocators.ADDED_ITEM)
+        item_name_text = self.browser.find_element(*ProductPageLocators.ITEM_NAME)
+        print(added_item_text.text)
+        print(item_name_text.text)
+        assert added_item_text.text == item_name_text.text, "Added items are different"
+
+    def should_be_correct_price(self):
+        message_basket_total = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE)
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        print(product_price.text)
+        print(message_basket_total.text)
+        assert product_price.text == message_basket_total.text, "Prices are different"

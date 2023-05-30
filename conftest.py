a37d2c75ad1d6d41aafb01ad657d73490b6057a9
AngelikaKh <a.khodzhaian@gmail.com>
2023-05-30 20:05:26 +0400

    adding TestUserAddToBasketFromProductPage class

--- conftest.py
+++ conftest.py
@@ -14,7 +14,6 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    browser = None
     if browser_name == "Chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
